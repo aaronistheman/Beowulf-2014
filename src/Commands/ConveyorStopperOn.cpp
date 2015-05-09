630539de5dbac0b79f116392a7d451d9a8ae8f6b
Diego-Andrade <bets636@gmail.com>
2015-05-09 09:08:51 -0700

    Implemented a command to set up the intake, conveyor, and shooter

--- src/Commands/ConveyorStopperOn.cpp
+++ src/Commands/ConveyorStopperOn.cpp
@@ -1,16 +1,27 @@
 
 #include "ConveyorStopperOn.h"
 
-ConveyorStopperOn::ConveyorStopperOn() {
+ConveyorStopperOn::ConveyorStopperOn()
+: timer()
+{
 	Requires(Robot::conveyor);
+	Requires(Robot::shooter);
+	Requires(Robot::intake);
 }
 
 void ConveyorStopperOn::Initialize() {
+	timer->Start();
 	Robot::conveyor->stopper->Set(DoubleSolenoid::kForward);
 }
 
 void ConveyorStopperOn::Execute() {
-	Robot::conveyor->conveyor->Set(-1.0);
+	Robot::shooter->shooterA->Set(1.0);
+	Robot::shooter->shooterB->Set(1.0);
+	if(timer->HasPeriodPassed(2.0))
+	{
+		Robot::conveyor->conveyor->Set(-1.0);
+		Robot::intake->intake->Set(-1.0);
+	}
 }
 
 bool ConveyorStopperOn::IsFinished() {
@@ -18,9 +29,13 @@ bool ConveyorStopperOn::IsFinished() {
 }
 
 void ConveyorStopperOn::End() {
+	timer->Stop();
+	timer->Reset();
 	Robot::conveyor->stopper->Set(DoubleSolenoid::kReverse);
 }
 
 void ConveyorStopperOn::Interrupted() {
+	timer->Stop();
+	timer->Reset();
 	Robot::conveyor->stopper->Set(DoubleSolenoid::kReverse);
 }
